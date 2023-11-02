ca8fcb6c5722825080d802adbfe5b1a1d503f320
deddyps <haicoders12@gmail.com>
2023-11-02 21:21:05 +0700

    remove unused element

--- components/Cart/index.js
+++ components/Cart/index.js
@@ -79,9 +79,6 @@ export default function Cart({ tokenVal }) {
                 <h1 className='text-lg font-bold text-gray-700 dark:text-gray-200 md:text-xl'>
                   SGD {item.retailPrice}
                 </h1>
-                <button className='px-2 py-1 text-xs font-bold text-white uppercase transition-colors duration-300 transform bg-gray-800 rounded dark:bg-gray-700 hover:bg-gray-700 dark:hover:bg-gray-600 focus:outline-none focus:bg-gray-700 dark:focus:bg-gray-600'>
-                  Add to Cart
-                </button>
               </div>
             </div>
           </div>
